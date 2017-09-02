7919c4e2016857f43ea231980c286e240c5d366f
Brian Hernandez <brianhernandez@users.noreply.github.com>
2017-09-02 19:32:22 -0400

    Implemented most of the new advanced accordion tabs and tweaking the responsiveness for it now.

--- searchmodaltrigger.js
+++ searchmodaltrigger.js
@@ -1,16 +1,15 @@
 (function() {
-  // Get element with .main-nav class and save it to menu variable
+  // Get element with #searc id and save it to searchBox variable
   var serachBox = document.querySelector('#search');
-  // Ensure menu exists and add a click event listener to it
-  console.log(serachBox);
+  // Ensure menu exists and add a keydown event listener to it
   serachBox && serachBox.addEventListener('keydown', function(event) {
-    console.log("Enter key pressed within searchbox.");
+    // Ensure corss browser compatability by checking and setting which
+    // event keyCode or which property to use
     var code = (event.keyCode ? event.keyCode : event.which);
-    // If the element that triggered the click is an A tag element...
-    console.log(code);
+    // If keydown event is triggered from the enter key (with a code of 13)...
     if (code == 13) {
-      // Find the element with the #offcanvas-toggle id and set its checked
-      // state to false to move the mobile menu back to its offcanvas state
+      // Find the element with the #searchbox-link id and click the link to
+      // trigger a target on the modal component
       document.querySelector('#searchbox-link').click();
     }
   });
